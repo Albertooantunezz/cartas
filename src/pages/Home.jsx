52892973cb2df59ee4ce195338034fa365c988d2
Albertooantunezz <aantunez@alu.ucam.edu>
2025-11-18 14:51:15 +0100

    opiniones fuera

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -295,7 +295,6 @@ export default function Home() {
         </div>
         {/* ---------- /Importador Moxfield ---------- */}
 
-        <Opinion />
       </div>
 
       {/* =======================
